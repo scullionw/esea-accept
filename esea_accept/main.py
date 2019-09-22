4d732b4d739e7b0a5d3e1e5105fed4d9d6e9aa3d
William Scullion <williamscullion@gmail.com>
2019-09-22 01:11:32 -0400

    first commit

---
+++ esea_accept/main.py
@@ -0,0 +1,25 @@
+import pyautogui
+import sys
+import time
+
+def main():
+    while True:
+        coords = locate("findmatch.png")
+        if coords is not None:
+            x, y = coords
+            pyautogui.click(x, y)
+            break
+        time.sleep(2)
+
+    print("Clicked!")
+
+def locate(image):
+    try:
+        return pyautogui.locateCenterOnScreen(image)  
+    except TypeError:
+        return None
+    
+
+if __name__ == "__main__":
+    sys.exit(main())
+
