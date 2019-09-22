d5ac53434ea4a01da5bc6e878865183c1c78a324
scullionw <williamscullion@gmail.com>
2019-09-22 19:02:44 -0400

    dont generate templates every time

--- esea_accept/main.py
+++ esea_accept/main.py
@@ -6,12 +6,15 @@ import numpy as np
 import mss
 import imutils
 
+BASE_SCALING = 0.25
+
 
 def main():
+    templates = generate_templates("argus.png")
     with mss.mss() as sct:
         while True:
             start = time.process_time()
-            coords = locate_fast_dpi_aware(sct, "argus.png")
+            coords = locate_fast_dpi_aware(sct, templates)
             print(time.process_time() - start)
             if coords is not None:
                 pyautogui.click(*coords)
@@ -28,24 +31,32 @@ def locate(image):
         return None
 
 
-def locate_fast_dpi_aware(sct, image, precision=0.7, base_scaling=0.25):
+def generate_templates(filename):
+    template_raw = cv2.imread(filename, 0)
+
+    templates = []
+    # Template scaling
+    for dpi_scale in [0.5, 0.625, 0.75, 0.875, 1]:
+        template = imutils.resize(
+            template_raw, width=int(template_raw.shape[1] * BASE_SCALING * dpi_scale)
+        )
+        template.shape[::-1]
+        templates.append((template, dpi_scale))
+
+    return templates
+
+
+def locate_fast_dpi_aware(sct, templates, precision=0.7):
     # Screenshot
     im = sct.grab(sct.monitors[0])
 
+    # Pre-process
     img_rgb = np.array(im)
-    img_rgb = imutils.resize(img_rgb, width=int(img_rgb.shape[1] * base_scaling))
+    img_rgb = imutils.resize(img_rgb, width=int(img_rgb.shape[1] * BASE_SCALING))
     img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 
-    # Template
-    template_raw = cv2.imread(image, 0)
-
     # Template scaling
-    for dpi_scale in [0.5, 0.625, 0.75, 0.875, 1]:
-
-        template = imutils.resize(
-            template_raw, width=int(template_raw.shape[1] * base_scaling * dpi_scale)
-        )
-        template.shape[::-1]
+    for template, dpi_scale in templates:
 
         # Matching
         res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
@@ -53,8 +64,8 @@ def locate_fast_dpi_aware(sct, image, precision=0.7, base_scaling=0.25):
 
         if max_val > precision:
             print(max_val, dpi_scale)
-            x, y = [int(c / base_scaling) for c in max_loc]
-            height, width = [int(d / base_scaling) for d in template.shape]
+            x, y = [int(c / BASE_SCALING) for c in max_loc]
+            height, width = [int(d / BASE_SCALING) for d in template.shape]
             return x + (width / 2), y + (height / 2)
 
     return None
